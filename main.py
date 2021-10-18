f103624412bebd6241332e8164d29dad48528e60
Joker-gr09 <1026116978@qq.com>
2021-10-18 18:58:17 +0800

    main

---
+++ main.py
@@ -0,0 +1,52 @@
+<<<<<<< HEAD
+# main page
+from Mode_1 import *
+from Mode_2 import *
+from Mode_3 import *
+
+
+def main():
+    print("Please enter your grade?")
+    grad = input("Grade:")
+
+    print("Please enter the number of questions?")
+    number_str = input("Number:")
+    number = int(number_str)
+
+    if grad == str(1) or grad == str(2):
+        calculation_mode_1(number)
+
+    elif grad == str(3) or grad == str(4):
+        calculation_mode_2(number)
+
+    elif grad == str(5) or grad == str(6):
+        calculation_mode_3(number)
+
+
+=======
+# main page
+from Mode_1 import *
+from Mode_2 import *
+from Mode_3 import *
+
+
+def main():
+    print("Please enter your grade?")
+    grad = input("Grade:")
+
+    print("Please enter the number of questions?")
+    number_str = input("Number:")
+    number = int(number_str)
+
+    if grad == str(1) or grad == str(2):
+        calculation_mode_1(number)
+
+    elif grad == str(3) or grad == str(4):
+        calculation_mode_2(number)
+
+    elif grad == str(5) or grad == str(6):
+        calculation_mode_3(number)
+
+
+>>>>>>> 1606e42352b77e8ed0b7e9478f204bf8b2186668
+main()
